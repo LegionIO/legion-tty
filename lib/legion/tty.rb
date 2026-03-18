1f9396db58451692c2f4d1653b91449bc4539fb1
Esity <matthewdiverson@gmail.com>
2026-03-17 23:50:22 -0500

    wire integration, add smoke test, finalize phase 1

--- lib/legion/tty.rb
+++ lib/legion/tty.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 require_relative 'tty/version'
+require_relative 'tty/theme'
+require_relative 'tty/hotkeys'
+require_relative 'tty/screen_manager'
+require_relative 'tty/screens/base'
+require_relative 'tty/components/digital_rain'
+require_relative 'tty/components/input_bar'
+require_relative 'tty/components/markdown_view'
+require_relative 'tty/components/message_stream'
+require_relative 'tty/components/status_bar'
+require_relative 'tty/components/tool_panel'
+require_relative 'tty/components/wizard_prompt'
+require_relative 'tty/background/scanner'
+require_relative 'tty/background/github_probe'
+require_relative 'tty/screens/onboarding'
+require_relative 'tty/screens/chat'
+require_relative 'tty/app'
 
 module Legion
   module TTY
---
+++ spec/legion/tty/integration_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tmpdir'
+
+RSpec.describe 'Legion::TTY integration' do
+  it 'loads all core modules' do
+    expect(defined?(Legion::TTY::App)).to be_truthy
+    expect(defined?(Legion::TTY::ScreenManager)).to be_truthy
+    expect(defined?(Legion::TTY::Theme)).to be_truthy
+    expect(defined?(Legion::TTY::Hotkeys)).to be_truthy
+  end
+
+  it 'loads all screens' do
+    expect(defined?(Legion::TTY::Screens::Base)).to be_truthy
+    expect(defined?(Legion::TTY::Screens::Chat)).to be_truthy
+    expect(defined?(Legion::TTY::Screens::Onboarding)).to be_truthy
+  end
+
+  it 'loads all components' do
+    expect(defined?(Legion::TTY::Components::DigitalRain)).to be_truthy
+    expect(defined?(Legion::TTY::Components::InputBar)).to be_truthy
+    expect(defined?(Legion::TTY::Components::MessageStream)).to be_truthy
+    expect(defined?(Legion::TTY::Components::StatusBar)).to be_truthy
+    expect(defined?(Legion::TTY::Components::ToolPanel)).to be_truthy
+    expect(defined?(Legion::TTY::Components::MarkdownView)).to be_truthy
+    expect(defined?(Legion::TTY::Components::WizardPrompt)).to be_truthy
+  end
+
+  it 'loads background modules' do
+    expect(defined?(Legion::TTY::Background::Scanner)).to be_truthy
+    expect(defined?(Legion::TTY::Background::GitHubProbe)).to be_truthy
+  end
+
+  it 'can instantiate App' do
+    Dir.mktmpdir do |dir|
+      app = Legion::TTY::App.new(config_dir: dir)
+      expect(app).to be_a(Legion::TTY::App)
+      expect(app.screen_manager).to be_a(Legion::TTY::ScreenManager)
+      expect(app.hotkeys).to be_a(Legion::TTY::Hotkeys)
+    end
+  end
+end
